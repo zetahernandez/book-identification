23f4bfdf20edaeceb5d772b05acc96ca6bd71c5a
Fernando Hernandez <zetahernandez@gmail.com>
2013-01-25 20:06:49 -0300

    test HBSearch

--- src/main/java/com/book/identification/Main.java
+++ src/main/java/com/book/identification/Main.java
@@ -8,9 +8,17 @@ import java.net.URISyntaxException;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.util.Version;
+import org.hibernate.search.FullTextSession;
 
+import com.book.identification.dao.DAOFactory;
+import com.book.identification.dao.VolumeInfoDAO;
 import com.book.identification.httpserver.JettyHttpServer;
-import com.book.identification.task.CreateTreeOfCategories;
+import com.book.identification.model.VolumeInfo;
 
 
 public class Main {
@@ -31,8 +39,19 @@ public class Main {
 //		} catch (InterruptedException e) {
 //			e.printStackTrace();
 //		}
+//		VolumeInfoDAO volumeInfoDAO = DAOFactory.getInstance().getVolumeInfoDAO();
+//		FullTextSession fts = org.hibernate.search.Search.getFullTextSession( volumeInfoDAO.getSession());
 //		
-////		
+//		QueryParser queryParser = new org.apache.lucene.queryParser.QueryParser(Version.LUCENE_31,"title",new StandardAnalyzer((Version.LUCENE_31)));
+//		try {
+//			Query query = queryParser.parse("title:javascript");
+//			fts.createFullTextQuery(query, VolumeInfo.class);
+//		} catch (ParseException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+//		fts.close();
+////	
 ////		
 //		new CreateTreeOfCategories().execute();
 //		
