ede6a236d638b8f864eafc787c9b7357fd69633f
Fernando Hernandez <zetahernandez@gmail.com>
2013-02-09 16:08:22 -0300

    remvo endless

--- book-search-main/src/main/webapp/scripts/main.js
+++ book-search-main/src/main/webapp/scripts/main.js
@@ -1,7 +1,7 @@
 (function(root){
 	require(["config"], function(config){
 		requirejs.config(config);
-		require(["App", "domReady!", "ember","ember_data","endless","inview"], function(App, doc, Ember,DS,endless,inview){
+		require(["App", "domReady!", "ember","ember_data","inview"], function(App, doc, Ember,DS,inview){
 			var app_name = config.app_name || "App";
 			root[app_name] = App = Ember.Application.create(App);
 			!App.isInitialized && App.initialize();
