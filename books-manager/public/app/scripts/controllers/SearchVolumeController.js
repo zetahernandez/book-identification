ec198dfb593d8f381f3e9503d4e539e1a9ad6790
zetahernandez <zetahernandez@gmail.com>
2013-08-02 23:57:17 -0300

    remove renamed files

--- books-manager/public/app/scripts/controllers/SearchVolumeController.js
+++
@@ -1,9 +0,0 @@
-BooksManager.SearchVolumeController = Ember.ObjectController.extend({
-  needs: ['volumeList'],
-  searchText: '',
-
-  searchVolumes: function () {
-    this.get('controllers.volumeList').searchVolumes(this.get('searchText'));
-  }
-
-});
